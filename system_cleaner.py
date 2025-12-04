6bd21fbfd8c3f3fbbc67c64ecf497dbc92e43d64
Nethan Nagendran <nethan.nagendran@gmail.com>
2025-12-03 23:16:01 -0500

    updated deleting temp folders

--- system_cleaner.py
+++ system_cleaner.py
@@ -19,7 +19,7 @@ class SystemCleaner:
     def __init__(self):
         pass
 
-    #TEMP FOLDERS
+    #LISTING FOLDERS
     def list_temp(self, folder_dict): 
         for name, path in folder_dict.items(): 
             if not path: 
@@ -39,23 +39,33 @@ class SystemCleaner:
                 except FileNotFoundError: 
                     print(f" Folder not found: {full_path}")
 
-    def delete_temp(self, path): 
-        for i in os.listdir(self.path): 
-            file_path = os.path.join(self.path, i)
-            
-            try: 
-                if os.path.isfile(file_path): 
-                    os.remove(file_path)
-                    print(f'Removed file {file_path}')
-                elif os.path.isdir(file_path): 
-                    shutil.rmtree(file_path)
-                    print(f'Removed directory {file_path}')
+    def delete_temp(self, folder_dict): 
+        for name, path in folder_dict.items(): 
 
-            except PermissionError: 
-                print(f'Skipped locked file or folder: {file_path}')
+            if not path: 
+                continue
 
+            else: 
+                full_path = os.path.expandvars(path)
+                print(f"\nDeleting contents of {name} ({full_path}):")
+
+                try: 
+                    for item in os.listdir(full_path): 
+                        item_path = os.path.join(full_path, item)
+                    try: 
+                        if os.path.isfile(item_path): 
+                            os.remove(item_path)
+                            print(f"Removed file: {item_path}")
+                        elif os.path.isdir(item_path): 
+                            shutil.rmtree(item_path)
+                            print(f"Removed directory: {item_path}")
+                    except PermissionError:
+                        print(f"Skipped locked items: {item_path}")
+                except PermissionError:
+                    print(f"Skipped locked folder: {full_path}")
+                except FileNotFoundError:
+                    print(f"Folder not found: {full_path}")
 
 cleaner = SystemCleaner() 
-# cleaner.list_temp()
-cleaner.list_temp(folderTargets)
+# cleaner.list_temp(folderTargets)
 # cleaner.delete_temp()
